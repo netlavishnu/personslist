937cbd39bab7f9152cf855fa9c25300f1d1b609d
Your Name <you@example.com>
2020-12-08 10:37:36 -0800

    validation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Label from "./components/Label";
+// import ValidationForm from "./components/ValidationForm";
 
 function App() {
   return (
     <div>
       <Label />
+      {/* <ValidationForm /> */}
     </div>
   );
 }
--- src/components/Label.js
+++ src/components/Label.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 export default class Persons extends Component {
   state = {
     personsList: [
@@ -6,57 +7,115 @@ export default class Persons extends Component {
       { name: "vishnu", fathername: "Jagan", age: 23, Gender: "male" },
       { name: "ramu", fathername: "Keshava", age: 21, Gender: "male" },
     ],
-    name: "",
-    fathername: "",
-    age: "",
-    Gender: "",
-    delete: "",
+    data: { name: "", fathername: "", age: "", Gender: "" },
+    currentId: "",
+    errors: {
+      name: "",
+      fathername: "",
+      age: "",
+      Gender: "",
+    },
   };
 
   nameInput = (e) => {
-    this.setState({ name: e.target.value });
+    this.setState({
+      data: { ...this.state.data, name: e.target.value },
+      errors: { ...this.state.errors, name: false },
+    });
   };
   fathernameInput = (e) => {
-    this.setState({ fathername: e.target.value });
+    this.setState({
+      data: { ...this.state.data, fathername: e.target.value },
+      errors: { ...this.state.errors, fathername: false },
+    });
   };
 
   ageInput = (e) => {
-    this.setState({ age: e.target.value });
+    this.setState({
+      data: { ...this.state.data, age: e.target.value },
+      errors: { ...this.state.errors, age: false },
+    });
   };
   genderInput = (e) => {
-    this.setState({ Gender: e.target.value });
+    this.setState({
+      data: { ...this.state.data, Gender: e.target.value },
+      errors: { ...this.state.errors, Gender: false },
+    });
+  };
+
+  validate = () => {
+    this.setState({
+      errors: {
+        name: this.state.data.name == "",
+        fathername: this.state.data.fathername == "",
+        age: this.state.data.age == "",
+        Gender: this.state.data.Gender == "",
+      },
+    });
+    return (
+      this.state.data.name !== "" &&
+      this.state.data.fathername !== "" &&
+      this.state.data.age !== "" &&
+      this.state.data.Gender !== ""
+    );
   };
 
   Submit = (e) => {
     e.preventDefault();
-    if (
-      this.state.name !== "" &&
-      this.state.fathername !== "" &&
-      this.state.age !== "" &&
-      this.state.Gender !== ""
-    ) {
-      if (this.state.value === "") {
-        console.log("please enter fields");
-      } else {
+
+    // const newPerson = {
+    //   name: this.state.name,
+    //   fathername: this.state.fathername,
+    //   age: this.state.age,
+    //   Gender: this.state.Gender,
+    // };
+
+    if (this.validate())
+      if (this.state.currentId == "") {
         console.log(this.state.value);
+
         this.setState({
-          personsList: [
-            ...this.state.personsList,
-            {
-              name: this.state.name,
-              fathername: this.state.fathername,
-              age: this.state.age,
-              Gender: this.state.Gender,
-              delete: this.state.delete,
-            },
-          ],
+          personsList: [...this.state.personsList, this.state.data],
+          data: {
+            name: "",
+            fathername: "",
+            age: "",
+            Gender: "",
+          },
         });
-        this.setState({ name: "", fathername: "", age: "", Gender: "" });
+      } else {
+        const newPersons = this.state.personsList.map((person, i) => {
+          if (i == this.state.currentId) {
+            return this.state.data;
+          } else {
+            return person;
+          }
+        });
+        this.setState({
+          personsList: newPersons,
+          currentId: "",
+          data: {
+            name: "",
+            fathername: "",
+            age: "",
+            Gender: "",
+          },
+        });
       }
-    } else {
-      alert("enter all fields");
-    }
   };
+  edit = (e) => {
+    e.preventDefault();
+    this.setState({
+      currentId: e.target.id,
+      data: {
+        name: this.state.personsList[e.target.id].name,
+        fathername: this.state.personsList[e.target.id].fathername,
+        age: this.state.personsList[e.target.id].age,
+        Gender: this.state.personsList[e.target.id].Gender,
+      },
+    });
+  };
+
   delete = (e) => {
     console.log(e.target.id);
     this.setState({
@@ -65,6 +124,7 @@ export default class Persons extends Component {
       ),
     });
   };
+
   render() {
     return (
       <div>
@@ -75,26 +135,34 @@ export default class Persons extends Component {
               type="text"
               class="form-control"
               placeholder="Enter name"
-              value={this.state.name}
+              value={this.state.data.name}
               onChange={this.nameInput}
             ></input>
+            <p className="error">
+              {this.state.errors.name ? "cant be blank" : ""}{" "}
+            </p>
             <label>fathername:</label>
             <input
               type="text"
               class="form-control"
               placeholder="Enter name"
-              value={this.state.fathername}
+              value={this.state.data.fathername}
               onChange={this.fathernameInput}
             ></input>
-
+            <p className="error">
+              {this.state.errors.fathername ? "cant be blank" : ""}{" "}
+            </p>
             <label>age:</label>
             <input
               type="number"
               class="form-control"
               placeholder="Enter age"
-              value={this.state.age}
+              value={this.state.data.age}
               onChange={this.ageInput}
             ></input>
+            <p className="error">
+              {this.state.errors.age ? "cant be blank" : ""}{" "}
+            </p>
             <label>Gender:</label>
             <div>
               <input
@@ -102,7 +170,7 @@ export default class Persons extends Component {
                 name="radio"
                 onClick={this.genderInput}
                 value="male"
-                checked={this.state.Gender === "male"}
+                checked={this.state.data.Gender === "male"}
               />
               <label>male</label>
             </div>
@@ -112,9 +180,12 @@ export default class Persons extends Component {
                 name="radio"
                 onClick={this.genderInput}
                 value="female"
-                checked={this.state.Gender === "female"}
+                checked={this.state.data.Gender === "female"}
               />
               <label>female</label>
+              <p className="error">
+                {this.state.errors.Gender ? "cant be blank" : ""}{" "}
+              </p>
             </div>
             <button className="btn btn-success" onClick={this.Submit}>
               Submit
@@ -131,6 +202,7 @@ export default class Persons extends Component {
                 <th>Father Name</th>
                 <th>age</th>
                 <th>Gender</th>
+                <th>Edit</th>
                 <th>delete</th>
               </tr>
             </thead>
@@ -142,6 +214,11 @@ export default class Persons extends Component {
                   <td>{p.fathername}</td>
                   <td>{p.age}</td>
                   <td>{p.Gender}</td>
+                  <td>
+                    <button id={i} onClick={this.edit} className="red">
+                      Edit
+                    </button>
+                  </td>
                   <td>
                     <button id={i} onClick={this.delete} className="red">
                       Delete
